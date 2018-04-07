adc4a274b3bdde076b2d6b631f344e713f45ab05
Vineeth Puli <vpuli98@gmail.com>
2018-04-07 09:43:13 -0400

    bitcaaaaaamp

--- BitCamp/BitCamp/ViewController.swift
+++ BitCamp/BitCamp/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let locationManager = CLLocationManager()
+        
+        locationManager.requestAlwaysAuthorization()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /*
+        StitchClientFactory
+            .create(appId: "<your-client-app-id>")
+            .done { (client: StitchClient) in
+                // Perform requests to Stitch using the variable "client",
+                // or assign the value of the variable "client" to some
+                // property accessible outside this closure.
+                
+                // For example, if this is in a class which has a
+                // stored StitchClient property named "stitchClient":
+                self.stitchClient = client
+            }.cauterize()
+        */
+ 
     }
 
     override func didReceiveMemoryWarning() {
